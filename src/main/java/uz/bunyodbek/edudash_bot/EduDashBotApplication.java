d6c4301bfd6412933767dc0e1a9e3eee1ed534a7
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2025-02-26 23:03:08 +0500

    add project

---
+++ src/main/java/uz/bunyodbek/edudash_bot/EduDashBotApplication.java
@@ -0,0 +1,13 @@
+package uz.bunyodbek.edudash_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduDashBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduDashBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/bunyodbek/edudash_bot/EduDashBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bunyodbek.edudash_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduDashBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
